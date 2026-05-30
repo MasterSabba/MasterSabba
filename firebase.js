62b8d728dbe3a2df46030e22e60585e3701b5c7d
MasterSabba <sabbadini.francesco@studenti.salesianichiari.it>
2026-05-30 15:42:45 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxZQF5OO5-znaipaPYeFcjOARjFXO1_gc",
+  authDomain: "mastersabba-games.firebaseapp.com",
+  projectId: "mastersabba-games",
+  storageBucket: "mastersabba-games.firebasestorage.app",
+  messagingSenderId: "254141019232",
+  appId: "1:254141019232:web:29702c03b91f8a76677f05",
+  measurementId: "G-775DFWZ6PC"
+};
+
+// Inizializza Firebase
+const app = initializeApp(firebaseConfig);
+
+// Esporta i moduli pronti per essere usati nell'index.html
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
