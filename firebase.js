807fd37179639c4e19a54acd51736b0c0fa4d157
MasterSabba <sabbadini.francesco@studenti.salesianichiari.it>
2026-05-30 15:45:29 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -17,5 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 // Esporta i moduli pronti per essere usati nell'index.html
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
